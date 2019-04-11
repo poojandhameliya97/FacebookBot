9c51bc501d9f1eb68f5e9e559c35f3fab24ea162
poojand001 <codehack009@gmail.com>
2019-04-11 18:51:27 +0530

    First

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -82,7 +83,7 @@ app.get('/', (req, res) => {
             
             name = bodyObj.first_name;
             data=checkMessageFormat(event.message.text);
-           // type=getLuisIntent(data[1])
+            type=getLuisIntent(data[1])
       //console.log(type);
         dataDB={
           "Location": data[0],
@@ -97,7 +98,7 @@ app.get('/', (req, res) => {
           "logoURL":  "http://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19751.png" 
         };
         //console.log(name);
-         // ref.push(dataDB);
+         ref.push(dataDB);
       }
     });
     //console.log(name);
