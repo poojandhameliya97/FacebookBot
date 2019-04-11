e1138bdea9465d730d34c39aa89aa2c6ad3a1cc3
poojand001 <codehack009@gmail.com>
2019-04-11 17:55:57 +0530

    First commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const request = require('request');
+var querystring = require('querystring');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -77,12 +78,14 @@ app.get('/', (req, res) => {
         } else{
             var bodyObj = JSON.parse(body)
             //console.log(bodyObj);
+            
             name = bodyObj.first_name;
             data=checkMessageFormat(event.message.text);
+            type=getLuisIntent(data[1])
       //console.log(data[0]);
         dataDB={
           "Location": data[0],
-          "Type": data[2],
+          "Type": type,
           "Body": data[1],
           "Timestamp": event.timestamp,
           "IssuerID": sender,
@@ -239,3 +242,53 @@ app.get('/', (req, res) => {
        
     }
   }
+
+
+  function getLuisIntent(utterance) {
+
+    // endpoint URL
+    var endpoint =
+        "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/";
+
+    // Set the LUIS_APP_ID environment variable 
+    // to df67dcdb-c37d-46af-88e1-8b97951ca1c2, which is the ID
+    // of a public sample application.    
+    var luisAppId = process.env.LUIS_APP_ID;
+
+    // Read LUIS key from environment file ".env"
+    // You can use the authoring key instead of the endpoint key. 
+    // The authoring key allows 1000 endpoint queries a month.
+    var endpointKey = process.env.LUIS_ENDPOINT_KEY;
+
+    // Create query string 
+    var queryParams = {
+        "verbose":  true,
+        "q": utterance,
+        "subscription-key": endpointKey
+    }
+
+    // append query string to endpoint URL
+    var luisRequest =
+        endpoint + luisAppId +
+        '?' + querystring.stringify(queryParams);
+
+    // HTTP Request
+    request(luisRequest,
+        function (err,
+            response, body) {
+
+            // HTTP Response
+            if (err)
+                console.log(err);
+            else {
+                var data = JSON.parse(body);
+
+                console.log(`Query: ${data.query}`);
+                console.log(`Top Intent: ${data.topScoringIntent.intent}`);
+                console.log('Intents:');
+                console.log(JSON.stringify(data.intents));
+                return data.topScoringIntent.intent;
+            }
+        });
+
+}
